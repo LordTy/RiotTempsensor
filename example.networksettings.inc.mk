2f5db0b9136129e613e58d16198215df669abc49
Tim Broenink <timbroenink@gmail.com>
2021-02-19 15:38:17 +0000

    Border router makefile

--- example.networksettings.inc.mk
+++
@@ -1,6 +0,0 @@
-# Network settings for the Temperature sensor nodes.
-# Override these with your own settings.
-
-DEFAULT_PAN_ID ?= 0x13 #default 0x13
-DEFAULT_CHANNEL ?= 26 #Between 11 and 26 (default 26)
-DEFAULT_HOST_IP ?= \"2001:db8::1\"
