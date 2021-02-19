70b45deb420c5066d0fa756322c970edce20c61a
Tim Broenink <timbroenink@gmail.com>
2021-02-19 15:03:04 +0000

    Initial content Commit

--- Makefile
+++ Makefile
@@ -1,18 +1,51 @@
 APPLICATION = sensors
 
 BOARD ?= nrf52840dongle
-PORT ?= /dev/ttyACM0
+PORT ?= /dev/ttyACM1
 
+# This has to be the absolute path to the RIOT base directory:
+RIOTBASE ?= $(CURDIR)/../../RIOT
+
+include networksettings.inc.mk
+
+
+
+# Increase from default for confirmable block2 follow-on requests
+GCOAP_RESEND_BUFS_MAX ?= 2
+CFLAGS += -DCONFIG_GCOAP_RESEND_BUFS_MAX=$(GCOAP_RESEND_BUFS_MAX)
+CFLAGS += -DHOSTIP6=$(DEFAULT_HOST_IP)
+
+# Include packages that pull up and auto-init the link layer.
+# NOTE: 6LoWPAN will be included if IEEE802.15.4 devices are present
+USEMODULE += gnrc_netdev_default
+USEMODULE += auto_init_gnrc_netif
+# Specify the mandatory networking modules
+USEMODULE += gnrc_ipv6_default
+USEMODULE += gcoap
+# Additional networking modules that can be dropped if not needed
+USEMODULE += gnrc_icmpv6_echo
+
+# Required by gcoap example
+USEMODULE += od
+USEMODULE += fmt
+# Add also the shell, some shell commands
 USEMODULE += shell
 USEMODULE += shell_commands
 USEMODULE += ps
-USEMODULE += xtimer
 
-# Add the required modules here
+# Import lps331ap driver module
 USEMODULE += bmp280_spi
-
 CFLAGS+= -DBMX280_PARAM_CS=GPIO_PIN\(0,17\) 
 
-RIOTBASE ?= $(CURDIR)/../../RIOT
+# Comment this out to disable code in RIOT that does safety checking
+# which is not needed in a production environment but helps in the
+# development process:
+DEVELHELP ?= 1
+
+# Change this to 0 show compiler invocation lines by default:
+QUIET ?= 1
 
 include $(RIOTBASE)/Makefile.include
+
+# Set a custom channel if needed
+include $(RIOTMAKE)/default-radio-settings.inc.mk
--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "net/af.h"
+#include "net/coap.h"
+#include "net/nanocoap.h"
 #include "thread.h"
 #include "timex.h"
 #include "xtimer.h"
@@ -9,15 +12,26 @@
 
 #include "mutex.h"
 
-
+#include "net/gcoap.h"
 
 #include "bmx280.h"
 #include "bmx280_params.h"
+
+#ifndef HOSTIP6
+    #define HOSTIP6 "2001:db8::1"
+#endif
 
 static bmx280_t bmp280;
 static mutex_t bmp_lock = MUTEX_INIT;
 
 static char bmp_stack[THREAD_STACKSIZE_MAIN];
+
+coap_pkt_t coap_pkt;
+uint8_t coap_buf[64];
+sock_udp_ep_t remote = {.family = AF_INET6,
+                        .port = 5863,
+                        .netif = SOCK_ADDR_ANY_NETIF};
+
 
 static void *bmp_thread(void *arg)
 {
@@ -26,16 +40,27 @@ static void *bmp_thread(void *arg)
     while (1) {
         /* Acquire the mutex here */
         mutex_lock(&bmp_lock);
-
         /* Read the temperature values here */
         int16_t temp = 0;
         temp = bmx280_read_temperature(&bmp280);
-        printf("Temperature: %i,%u C\n",(temp/100),(temp%100));
+        printf("Temperature: %i.%u C\n",(temp/100),(temp%100));
+
+
+        /* Send temperature as coap message */
+        size_t len;
+       gcoap_req_init(&coap_pkt, coap_buf, sizeof(coap_buf), COAP_METHOD_POST, "/temp");
+       len = coap_opt_finish(&coap_pkt, COAP_OPT_FINISH_PAYLOAD);
+       char msg[32]={'\0'};
+       sprintf(msg,"%02i.%02u",(temp/100),(temp%100));
+       strcpy((char*)coap_pkt.payload,msg);
+       len+=strlen(msg);
+       gcoap_req_send(coap_buf, len,
+                        &remote, NULL, NULL);
+
 
         /* Release the mutex here */
         mutex_unlock(&bmp_lock);
-
-        xtimer_usleep(5 * US_PER_SEC);
+        xtimer_sleep(60);
     }
 
     return 0;
@@ -50,7 +75,7 @@ static int bmp280_handler(int argc, char *argv[]){
     if (!strcmp(argv[1],"temperature")){
         int16_t temp = 0;
         temp = bmx280_read_temperature(&bmp280);
-        printf("Temperature: %i,%u C\n",(temp/100),(temp%100));
+        printf("Temperature: %i.%u C\n",(temp/100),(temp%100));
     }
     else if (!strcmp(argv[1],"pressure")){
         uint32_t pres = 0;
@@ -85,8 +110,12 @@ static const shell_command_t commands[] = {
     { NULL, NULL, NULL}
 };
 
+#define MAIN_QUEUE_SIZE (4)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
 int main(void)
 {
+
     /* Initialize the bmp280 sensor here */
     int err = bmx280_init(&bmp280, bmx280_params);
     if(err==BMX280_OK){
@@ -97,8 +126,15 @@ int main(void)
         printf("BMP280 device not found\n");
     }
 
+    printf("Coap target is: %s",HOSTIP6);
+    ipv6_addr_t addr;
+    ipv6_addr_from_str(&addr, HOSTIP6);
+    memcpy(&remote.addr.ipv6[0], &addr.u8[0], sizeof(addr.u8));
+
     thread_create(bmp_stack, sizeof(bmp_stack), THREAD_PRIORITY_MAIN - 1,
                   0, bmp_thread, NULL, "bmp280");
+    
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
     /* Everything is ready, let's start the shell now */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
